031483eef42bf8b4d6ef0fb4b30766e8c562a75c
basement96 <kusohira.scp3026@gmail.com>
2026-08-01 16:11:31 +0900

    Delete searchMaimai/bookmarklet.js

--- searchMaimai/bookmarklet.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){var s=document.createElement('script');s.src=(location.protocol+'//'+location.host+location.pathname.replace(/[^/]+$/,'')+'searchMaimaiSystem.js');document.body.appendChild(s);})();
